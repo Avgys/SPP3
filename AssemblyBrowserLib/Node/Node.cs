e6dcc5c588728c6a486b1643d0bffa0686a656a8
Ilya Kosykh <ilyuha.prosto.22041961@gmail.com>
2021-10-31 21:26:48 +0300

    Added library

---
+++ AssemblyBrowserLib/Node/Node.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser.Lib.Node
+{
+    public class Node : INode
+    {
+        public string Optional { get; set; }
+        public string NodeType { get; set; }
+        public string AccessModifier { get; set; }
+        public string TypeModifier { get; set; }
+        public string ClassType { get; set; }
+        public string Type { get; set; }
+        public string FullType { get; set; }
+        public string ReturnType { get; set; }
+        public string Name { get; set; }
+        public List<INode> Nodes { get; } = new();
+
+        public Node
+        (
+            string nodeType,
+            string optional = "",
+            string accessModifier = "",
+            string typeModifier = "",
+            string classType = "",
+            string type = "",
+            string fullType = "",
+            string returnType = "",
+            string name = "",
+            IEnumerable<INode> nodes = null
+        )
+        {
+            Optional = optional;
+            NodeType = nodeType;
+            AccessModifier = accessModifier;
+            TypeModifier = typeModifier;
+            ClassType = classType;
+            Type = type;
+            FullType = fullType;
+            ReturnType = returnType;
+            Name = name;
+            AddRange(nodes);
+        }
+
+        public void AddNode(INode node)
+        {
+            if (node != null)
+            {
+                Nodes.Add(node);
+            }
+        }
+
+        public void AddRange(IEnumerable<INode> nodes)
+        {
+            if (nodes != null)
+            {
+                Nodes.AddRange(nodes);
+            }
+        }
+    }
+}
